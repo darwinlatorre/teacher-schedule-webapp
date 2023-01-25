969b0b3fe7cb26363a251ded808d60afb63c55ef
Juan-MZ <juanjosz@unicauca.edu.co>
2023-01-25 00:05:27 -0500

    client: update in routes

--- client/src/routes/coordinator.js
+++ client/src/routes/coordinator.js
@@ -1,4 +1,8 @@
 import HomePage from "../features/coordinator/home.js";
+import EnviromentHomePage from "../features/coordinator/enviromentManagment/enviromentHome.js"
+import PeriodHomePage from "../features/coordinator/periodManagment/periodHome.js"
+import ScheduleHomePage from "../features/coordinator/scheduleManagment/scheduleHome.js"
+import TeacherHomePage from "../features/coordinator/teacherManagment/teacherHome.js"
 
 /**
  * rutas privadas del coordinador
@@ -7,5 +11,21 @@ export const coordinator = [
   {
     path: '/coordinator/home',
     element: <HomePage />
+  },
+  {
+    path: '/coordinator/enviroment/home',
+    element: <EnviromentHomePage />
+  },
+  {
+    path: '/coordinator/period/home',
+    element: <PeriodHomePage />
+  },
+  {
+    path: '/coordinator/schedule/home',
+    element: <ScheduleHomePage />
+  },
+  {
+    path: '/coordinator/teacher/home',
+    element: <TeacherHomePage />
   }
 ]
---
+++ client/src/routes/public.js
@@ -0,0 +1,11 @@
+import Login from "../features/login/login.js";
+
+/**
+ * rutas publicas
+ */
+export const publicas = [
+  {
+    path: '/login',
+    element: <Login />
+  }
+]
